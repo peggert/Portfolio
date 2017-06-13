45e33826ea2acc8dc623d3f70ff1182c1513159a
Philipp Eggert <16015355@student.hhs.nl>
2017-06-13 19:46:46 +0200

    Final Page
    Portfolio

---
+++ script/mainJava.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+document.getElementById("submit").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("mailMeForm").innerHTML = "Your Mail has reached my Inbox. I will write back to you within the next 2 days! Thank you!";
+}
